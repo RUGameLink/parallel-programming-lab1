d171780cd6f796893025cfaca77ca5e5da821610
BlackStreit <mix06062001@bk.ru>
2022-10-11 11:23:01 +0800

    Плюю готовой работой

--- pp/Program.cs
+++ pp/Program.cs
@@ -1,4 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int elementCount;
+        int threadCount;
+        Console.WriteLine("Введите кол-во потоков");
+        while (!int.TryParse(Console.ReadLine(), out threadCount)
+            || (threadCount < 4))
+        {
+            Console.WriteLine("Число введено неверно. Попробуйте ещё раз");
+        }
+        Console.WriteLine("Введите кол-во элементов");
+        while (!int.TryParse(Console.ReadLine(), out elementCount)
+            || (elementCount < 1))
+        {
+            Console.WriteLine("Число введено неверно. Попробуйте ещё раз");
+        }
+        int[] array = new int[elementCount];
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 16);
+        }
+        int[] returns = new int[threadCount];
+        Thread[] threads = new Thread[threadCount];
+        for (int i = 0; i < threads.Length; i++)
+        {
 
+            threads[i] = new Thread(() => { returns[i] = (ThreadArray(array, i, threadCount)); });
+            threads[i].Start();
+            threads[i].Join();
 
-Console.WriteLine("Git, ты сволочь!");
+        }
+        int multEven = 1; 
+        for(int i = 0; i < returns.Length; i++)
+        {
+            multEven *= returns[i];
+        }
+        Console.WriteLine($"Произведение четных элементов: {multEven}");
+        Console.ReadKey();
+    }
+
+    static int ThreadArray(object? paramArray, object? totalI, object? threadCount)
+    {
+        int beg, h, end;
+        int multEven = 1;
+        int[]? array = paramArray as int[];
+        int nt = Convert.ToInt32(totalI);
+        int count = Convert.ToInt32(threadCount);
+        h = array.Length / count;
+        beg = h * nt; end = beg + h;
+        if (nt == count - 1)
+        {
+            end = array.Length;
+        }
+        for (int i = beg; i < end; i++)
+        {
+            if(array[i] % 2 == 0)
+            {
+                multEven *= array[i];
+            }
+        }
+        return multEven;
+    }
+}
